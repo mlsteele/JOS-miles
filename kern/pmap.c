d105aa32f2aedbc2096ba8e13bf583a9635e4630
Miles Steele <miles@milessteele.com>
2014-10-15 14:27:36 -0400

    implement mem_init_mp

--- kern/pmap.c
+++ kern/pmap.c
@@ -283,7 +283,15 @@ mem_init_mp(void)
 	//     Permissions: kernel RW, user NONE
 	//
 	// LAB 4: Your code here:
+    physaddr_t kstacktop_pa_i;
+    uintptr_t kstacktop_va_i;
+    int i;
+    for (i = 0; i < NCPU; i++) {
+        kstacktop_pa_i = PADDR(percpu_kstacks[i]);
+        kstacktop_va_i = KSTACKTOP - i * (KSTKSIZE + KSTKGAP);
 
+        boot_map_region(kern_pgdir, kstacktop_va_i-KSTKSIZE, KSTKSIZE, kstacktop_pa_i, PTE_W);
+    }
 }
 
 // --------------------------------------------------------------
