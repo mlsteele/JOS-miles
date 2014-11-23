c2902168894f5fd1869bfa4ca86b9629d7748daf
Miles Steele <miles@milessteele.com>
2014-11-22 21:45:53 -0500

    remove packet test from user/hello

--- user/hello.c
+++ user/hello.c
@@ -2,27 +2,8 @@
 #include <inc/lib.h>
 
 void
-packet_send_test()
-{
-    int r;
-    uint8_t buf[16];
-
-    memset(buf, 0, 16);
-
-    buf[0] = 0xA;
-    buf[1] = 0xB;
-    buf[7] = 0xC;
-    buf[8] = 0xD;
-
-    if ((r = sys_packet_transmit(&buf, 8)) < 0) {
-        panic("send failed: %d\n", r);
-    }
-}
-
-void
 umain(int argc, char **argv)
 {
 	cprintf("hello, world\n");
-    packet_send_test();
 	cprintf("i am environment %08x\n", thisenv->env_id);
 }
